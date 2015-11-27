f4c761955d851c6bf4023dcbeb945bbbf7172948
Tommy Troy Lin <tommytroylin@gmail.com>
2015-11-28 03:21:55 +0800

    Create 2 utilities fetch-util and id-generator

---
+++ app/utils/fetch-util.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Tommy on 11/26/15.
+ */
+//handle the http status
+export function checkHttpStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    } else {
+        var error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+
+}
+
+//parse the response into JSON
+export function parseJSON(response) {
+    return response.json()
+}
---
+++ app/utils/id-generator.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Tommy on 11/27/15.
+ */
+
+//
+export default class IdGenerator {
+
+    static config(config) {
+        IdGenerator.increment = config.increment ? config.increment : IdGenerator.increment;
+        IdGenerator.resultGenFunc = config.resultGenFunc ? config.resultGenFunc : IdGenerator.resultGenFunc;
+    }
+
+    // a function: (previousValue) => nextValue
+    static increment(previousValue) {
+        return ++previousValue;
+    }
+
+    //
+    static resultGenFunc(id, value) {
+        return id.toString() + value.toString();
+    }
+
+    // reset all generator in storage
+    static resetAll() {
+        throw new Error('resetAll() has not implemented yet')
+    }
+
+    // delete all generators
+    static cleanAll() {
+        IdGenerator.__generators = {};
+    }
+
+    // get a generator by id, init it if can't find it
+    static get(id, initValue) {
+        if (IdGenerator.__generators.hasOwnProperty(id)) {
+
+            return IdGenerator.__generators.id;
+        } else {
+            return new IdGenerator(id, initValue);
+        }
+    }
+
+    constructor(id, initValue) {
+        this.id = id;
+        this.initValue = initValue ? initValue : 0;
+        this.counter = this.initValue;
+        Object.assign(IdGenerator.__generators, {id: this});
+    }
+
+    next() {
+        this.counter = IdGenerator.increment(this.counter);
+        return IdGenerator.resultGenFunc(this.id, this.counter);
+    }
+    
+    now() {
+        return this.counter;
+    }
+
+    reset() {
+        this.counter = this.initValue;
+        return this;
+    }
+
+    clean() {
+        delete IdGenerator.__generators[this.id];
+    }
+
+};
+
+IdGenerator.__generators = {};
