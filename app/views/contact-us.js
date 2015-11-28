541533c3789b4facb0924fbc1cb8f85953d37243
Tommy Troy Lin <tommytroylin@gmail.com>
2015-11-29 01:59:12 +0800

    Update some unfinished views

--- app/views/contact-us.js
+++ app/views/contact-us.js
@@ -1,11 +1,51 @@
 import React from 'react';
-
+import { Grid, Row, Col, Panel, Button, Input} from 'react-bootstrap';
+import * as apiAddress from '../utils/api-address'
 
 export default class CHMSContactUsView extends React.Component {
 
     render() {
+        const pannelHeader = <h3>Contact Form</h3>;
+        const pannelFooter = (<div>
+
+        </div>);
         return (
-            <h1>Here is Tommy the developer</h1>
+            <div>
+
+                <Row>
+                    <Col xs={12} md={10} mdPush={1} lg={8} lgPush={2}>
+                        <Panel header={pannelHeader}>
+                            <form action={apiAddress.contactUsForm} method="post">
+                                <Grid fluid>
+                                    <Row>
+                                        <Col xs={12} md={6}>
+                                            <Input type="text" label="Your Name" placeholder="Enter your name"/>
+                                        </Col>
+                                        <Col xs={12} md={6}>
+                                            <Input type="email" label="Your Email"
+                                                   placeholder="Enter your email address"/>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col xs={12}>
+                                            <Input type="textarea" label="Your Message" placeholder="..."/>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col xs={12}>
+                                            <div className="pull-right"><Button bsStyle="danger" bsSize="small"
+                                                                                type="reset">Reset</Button> <Button
+                                                bsStyle="success" bsSize="small" type="button">Send</Button></div>
+                                        </Col>
+                                    </Row>
+                                </Grid>
+                            </form>
+                        </Panel>
+                    </Col>
+                    <hr />
+
+                </Row>
+            </div>
         );
     }
 
--- app/views/deep-mining.js
+++ app/views/deep-mining.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Grid, Row, Col, Well } from 'react-bootstrap';
 
 
 export default class CHMSDeepMiningView extends React.Component {
 
     render() {
         return (
-            <h1>Here is deepMining</h1>
+            <div>
+
+                <Row>
+                    <Col xs={12} sm={6}>
+                        <h3>3-Cluster</h3>
+                        <Well></Well>
+                    </Col>
+                    <Col xs={12} sm={6}>
+                        <h3>Obesity</h3>
+                        <Well></Well>
+                    </Col>
+                </Row>
+            </div>
         );
     }
 
