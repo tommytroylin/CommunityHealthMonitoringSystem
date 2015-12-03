59e403d9051412f2b4dad9b57fe740e8d0b371ea
Tommy Troy Lin <tommytroylin@gmail.com>
2015-12-04 01:40:24 +0800

    Add a double ring to the knob in indexShow

--- app/components/indexShow.js
+++ app/components/indexShow.js
@@ -7,7 +7,24 @@ import {CHMSIndexBarChart} from './charts';
 export default class CHMSIndexShow extends React.Component {
 
     componentDidMount() {
-        (window.$('#indexKnobInject')).knob({angleOffset: -110, angleArc: 220, fgColor: "#66CC66", height: 135});
+        (window.$('.indexKnobOuter')).knob({
+            angleOffset: -110,
+            angleArc: 220,
+            fgColor: "#66CC66",
+            height: 135,
+            width: 200,
+            thickness: .2,
+            displayInput: false
+        });
+        (window.$('.indexKnobInner')).knob({
+            angleOffset: -110,
+            angleArc: 220,
+            fgColor: "#66CC66",
+            height: 135,
+            width: 160,
+            thickness: .3,
+            displayInput: false
+        });
     }
 
     render() {
@@ -40,11 +57,22 @@ export default class CHMSIndexShow extends React.Component {
                     <Col xs={8} md={8} lg={7} lgPush={1}>
                         <h2 className="">Sentimental Analysis</h2>
                         <p className="lead">This is the knob about sentimental level for two types of cities,
-                            the cities with faster life pace and the cities with slower life pace.  </p>
+                            the cities with faster life pace and the cities with slower life pace. </p>
                     </Col>
-                    <div>
-                        <input id="indexKnobInject" type="text" value="75" className="dial" readOnly/>
-                    </div>
+                    <Col xs={4} lg={3} lgPush={1}>
+                        <div style={{position:"relative",height:135,margin:'auto'}}>
+
+                            <div style={{position:"absolute",left:"10px",top:"10px"}}>
+                                <input className="knob indexKnobOuter" value="80" readOnly/>
+                            </div>
+                            <div style={{position:"absolute",left:"30px",top:"30px"}}>
+                                <input className="knob indexKnobInner" value="100" readOnly/>
+                            </div>
+                            <div style={{position:"absolute",left:"87px",top:"100px"}}>
+                                <input type="text" className="knob" value="85/100" readOnly style={{width:'50px'}}/>
+                            </div>
+                        </div>
+                    </Col>
                 </Row>
 
             </Grid>
